a90a94ba6b396e8920ba2bfb4d7a41263c886dd0
faux123 <reioux@gmail.com>
2013-01-07 19:33:55 -0600

    intelli_plug: tweak for faster wakeup from suspend
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- arch/arm/mach-msm/intelli_plug.c
+++ arch/arm/mach-msm/intelli_plug.c
@@ -1,11 +1,7 @@
 /*
  * Author: Paul Reioux aka Faux123 <reioux@gmail.com>
  *
-<<<<<<< HEAD
- * Copyright 2012~2014 Paul Reioux
-=======
  * Copyright 2012 Paul Reioux
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -23,65 +19,24 @@
 #include <linux/sched.h>
 #include <linux/mutex.h>
 #include <linux/module.h>
-<<<<<<< HEAD
-#include <linux/rq_stats.h>
-#include <linux/slab.h>
-#include <linux/input.h>
 
-//#define DEBUG_INTELLI_PLUG
-#undef DEBUG_INTELLI_PLUG
+#define INTELLI_PLUG_MAJOR_VERSION	1
+#define INTELLI_PLUG_MINOR_VERSION	1
 
-#define INTELLI_PLUG_MAJOR_VERSION	2
-#define INTELLI_PLUG_MINOR_VERSION	2
-
-#define DEF_SAMPLING_MS			(500)
-#define BUSY_SAMPLING_MS		(250)
-
-#define DUAL_CORE_PERSISTENCE		14
-#define TRI_CORE_PERSISTENCE		10
-#define QUAD_CORE_PERSISTENCE		6
-
-#define BUSY_PERSISTENCE		20
-
-#define RUN_QUEUE_THRESHOLD		38
-
-#define CPU_DOWN_FACTOR			2
-
-static DEFINE_MUTEX(intelli_plug_mutex);
-
-static struct delayed_work intelli_plug_work;
-static struct delayed_work intelli_plug_boost;
-
-static struct workqueue_struct *intelliplug_wq;
-static struct workqueue_struct *intelliplug_boost_wq;
-
-static unsigned int intelli_plug_active = 0;
-=======
-
-#define INTELLI_PLUG_VERSION	1
-
-#define DEF_SAMPLING_RATE			(50000)
+#define DEF_SAMPLING_RATE		(50000)
+#define DEF_SAMPLING_MS			(50)
 
 static DEFINE_MUTEX(intelli_plug_mutex);
 
 struct delayed_work intelli_plug_work;
 
 static unsigned int intelli_plug_active = 1;
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 module_param(intelli_plug_active, uint, 0644);
 
 static unsigned int eco_mode_active = 0;
 module_param(eco_mode_active, uint, 0644);
 
-<<<<<<< HEAD
-static unsigned int sampling_time = 0;
-
 static unsigned int persist_count = 0;
-static unsigned int busy_persist_count = 0;
-
-=======
-static unsigned int persist_count = 0;
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 static bool suspended = false;
 
 #define NR_FSHIFT	3
@@ -103,59 +58,6 @@ module_param(nr_run_hysteresis, uint, 0644);
 
 static unsigned int nr_run_last;
 
-<<<<<<< HEAD
-static unsigned int NwNs_Threshold[] = { 19, 30,  19,  11,  19,  11, 0,  11};
-static unsigned int TwTs_Threshold[] = {140,  0, 140, 190, 140, 190, 0, 190};
-
-static int mp_decision(void)
-{
-	static bool first_call = true;
-	int new_state = 0;
-	int nr_cpu_online;
-	int index;
-	unsigned int rq_depth;
-	static cputime64_t total_time = 0;
-	static cputime64_t last_time;
-	cputime64_t current_time;
-	cputime64_t this_time = 0;
-
-	current_time = ktime_to_ms(ktime_get());
-	if (first_call) {
-		first_call = false;
-	} else {
-		this_time = current_time - last_time;
-	}
-	total_time += this_time;
-
-	rq_depth = rq_info.rq_avg;
-	//pr_info(" rq_deptch = %u", rq_depth);
-	nr_cpu_online = num_online_cpus();
-
-	if (nr_cpu_online) {
-		index = (nr_cpu_online - 1) * 2;
-		if ((nr_cpu_online < 4) &&
-			(rq_depth >= NwNs_Threshold[index])) {
-			if (total_time >= TwTs_Threshold[index]) {
-				new_state = 1;
-			}
-		} else if (rq_depth <= NwNs_Threshold[index+1]) {
-			if (total_time >= TwTs_Threshold[index+1] ) {
-				new_state = 0;
-			}
-		} else {
-			total_time = 0;
-		}
-	} else {
-		total_time = 0;
-	}
-
-	last_time = ktime_to_ms(ktime_get());
-
-	return new_state;
-}
-
-=======
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 static unsigned int calculate_thread_stats(void)
 {
 	unsigned int avg_nr_run = avg_nr_running();
@@ -166,25 +68,13 @@ static unsigned int calculate_thread_stats(void)
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_full);
 		nr_run_hysteresis = 8;
 		nr_fshift = 3;
-<<<<<<< HEAD
-#ifdef DEBUG_INTELLI_PLUG
-		pr_info("intelliplug: full mode active!");
-#endif
-=======
 		//pr_info("intelliplug: full mode active!");
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 	}
 	else {
 		threshold_size =  ARRAY_SIZE(nr_run_thresholds_eco);
 		nr_run_hysteresis = 4;
 		nr_fshift = 1;
-<<<<<<< HEAD
-#ifdef DEBUG_INTELLI_PLUG
-		pr_info("intelliplug: eco mode active!");
-#endif
-=======
 		//pr_info("intelliplug: eco mode active!");
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 	}
 
 	for (nr_run = 1; nr_run < threshold_size; nr_run++) {
@@ -204,134 +94,6 @@ static unsigned int calculate_thread_stats(void)
 	return nr_run;
 }
 
-<<<<<<< HEAD
-static void __cpuinit intelli_plug_boost_fn(struct work_struct *work)
-{
-
-	int nr_cpus = num_online_cpus();
-
-	if (nr_cpus < 2)
-		cpu_up(1);
-}
-
-static void __cpuinit intelli_plug_work_fn(struct work_struct *work)
-{
-	unsigned int nr_run_stat;
-	unsigned int cpu_count = 0;
-	unsigned int nr_cpus = 0;
-
-	int decision = 0;
-	int i;
-
-	if (intelli_plug_active == 1) {
-		nr_run_stat = calculate_thread_stats();
-#ifdef DEBUG_INTELLI_PLUG
-		pr_info("nr_run_stat: %u\n", nr_run_stat);
-#endif
-		cpu_count = nr_run_stat;
-		// detect artificial loads or constant loads
-		// using msm rqstats
-		nr_cpus = num_online_cpus();
-		if (!eco_mode_active && (nr_cpus >= 1 && nr_cpus < 4)) {
-			decision = mp_decision();
-			if (decision) {
-				switch (nr_cpus) {
-				case 2:
-					cpu_count = 3;
-#ifdef DEBUG_INTELLI_PLUG
-					pr_info("nr_run(2) => %u\n", nr_run_stat);
-#endif
-					break;
-				case 3:
-					cpu_count = 4;
-#ifdef DEBUG_INTELLI_PLUG
-					pr_info("nr_run(3) => %u\n", nr_run_stat);
-#endif
-					break;
-				}
-			}
-		}
-		/* it's busy.. lets help it a bit */
-		if (cpu_count > 2) {
-			if (busy_persist_count == 0) {
-				sampling_time = BUSY_SAMPLING_MS;
-				busy_persist_count = BUSY_PERSISTENCE;
-			}
-		} else {
-			if (busy_persist_count > 0)
-				busy_persist_count--;
-			else
-				sampling_time = DEF_SAMPLING_MS;
-		}
-
-		if (!suspended) {
-			switch (cpu_count) {
-			case 1:
-				if (persist_count > 0)
-					persist_count--;
-				if (persist_count == 0) {
-					//take down everyone
-					for (i = 3; i > 0; i--)
-						cpu_down(i);
-				}
-#ifdef DEBUG_INTELLI_PLUG
-				pr_info("case 1: %u\n", persist_count);
-#endif
-				break;
-			case 2:
-				persist_count = DUAL_CORE_PERSISTENCE;
-				if (!decision)
-					persist_count = DUAL_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-				if (nr_cpus < 2) {
-					for (i = 1; i < cpu_count; i++)
-						cpu_up(i);
-				} else {
-					for (i = 3; i >  1; i--)
-						cpu_down(i);
-				}
-#ifdef DEBUG_INTELLI_PLUG
-				pr_info("case 2: %u\n", persist_count);
-#endif
-				break;
-			case 3:
-				persist_count = TRI_CORE_PERSISTENCE;
-				if (!decision)
-					persist_count = TRI_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-				if (nr_cpus < 3) {
-					for (i = 1; i < cpu_count; i++)
-						cpu_up(i);
-				} else {
-					for (i = 3; i > 2; i--)
-						cpu_down(i);
-				}
-#ifdef DEBUG_INTELLI_PLUG
-				pr_info("case 3: %u\n", persist_count);
-#endif
-				break;
-			case 4:
-				persist_count = QUAD_CORE_PERSISTENCE;
-				if (!decision)
-					persist_count = QUAD_CORE_PERSISTENCE / CPU_DOWN_FACTOR;
-				if (nr_cpus < 4)
-					for (i = 1; i < cpu_count; i++)
-						cpu_up(i);
-#ifdef DEBUG_INTELLI_PLUG
-				pr_info("case 4: %u\n", persist_count);
-#endif
-				break;
-			default:
-				pr_err("Run Stat Error: Bad value %u\n", nr_run_stat);
-				break;
-			}
-		}
-#ifdef DEBUG_INTELLI_PLUG
-		else
-			pr_info("intelli_plug is suspened!\n");
-#endif
-	}
-	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
-		msecs_to_jiffies(sampling_time));
-=======
 static void intelli_plug_work_fn(struct work_struct *work)
 {
 	unsigned int nr_run_stat;
@@ -384,42 +146,37 @@ static void intelli_plug_work_fn(struct work_struct *work)
 		} //else
 			//pr_info("intelli_plug is suspened!\n");
 	}
-	schedule_delayed_work_on(0, &intelli_plug_work, msecs_to_jiffies(50));
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
+	schedule_delayed_work_on(0, &intelli_plug_work,
+		msecs_to_jiffies(DEF_SAMPLING_MS));
 }
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 static void intelli_plug_early_suspend(struct early_suspend *handler)
 {
-<<<<<<< HEAD
 	int i;
-	int num_of_active_cores = num_possible_cpus();
-	
-	flush_workqueue(intelliplug_wq);
-=======
+	int num_of_active_cores = 4;
 	
 	cancel_delayed_work_sync(&intelli_plug_work);
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 
 	mutex_lock(&intelli_plug_mutex);
 	suspended = true;
 	mutex_unlock(&intelli_plug_mutex);
 
 	// put rest of the cores to sleep!
-<<<<<<< HEAD
-	for (i = num_of_active_cores - 1; i > 0; i--) {
-		cpu_down(i);
+	for (i=1; i<num_of_active_cores; i++) {
+		if (cpu_online(i))
+			cpu_down(i);
 	}
 }
 
-static void __cpuinit intelli_plug_late_resume(struct early_suspend *handler)
+static void intelli_plug_late_resume(struct early_suspend *handler)
 {
 	int num_of_active_cores;
 	int i;
 
 	mutex_lock(&intelli_plug_mutex);
 	/* keep cores awake long enough for faster wake up */
-	persist_count = BUSY_PERSISTENCE;
+	persist_count = 27;
 	suspended = false;
 	mutex_unlock(&intelli_plug_mutex);
 
@@ -427,148 +184,24 @@ static void __cpuinit intelli_plug_late_resume(struct early_suspend *handler)
 	if (eco_mode_active)
 		num_of_active_cores = 2;
 	else
-		num_of_active_cores = num_possible_cpus();
+		num_of_active_cores = 4;
 
-	for (i = 1; i < num_of_active_cores; i++) {
-		cpu_up(i);
+	for (i=1; i<num_of_active_cores; i++) {
+		if (!cpu_online(i))
+			cpu_up(i);
 	}
 
-	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
+	schedule_delayed_work_on(0, &intelli_plug_work,
 		msecs_to_jiffies(10));
 }
 
-static struct early_suspend intelli_plug_early_suspend_struct_driver = {
-=======
-	switch (num_online_cpus()) {
-		case 4:
-			cpu_down(3);
-		case 3:
-			cpu_down(2);
-		case 2:
-			cpu_down(1);
-	}
-}
-
-static void intelli_plug_late_resume(struct early_suspend *handler)
-{
-	mutex_lock(&intelli_plug_mutex);
-	suspended = false;
-	mutex_unlock(&intelli_plug_mutex);
-
-	schedule_delayed_work_on(0, &intelli_plug_work, msecs_to_jiffies(10));
-}
-
 static struct early_suspend intelli_plug_early_suspend_struct = {
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 	.level = EARLY_SUSPEND_LEVEL_DISABLE_FB + 10,
 	.suspend = intelli_plug_early_suspend,
 	.resume = intelli_plug_late_resume,
 };
 #endif	/* CONFIG_HAS_EARLYSUSPEND */
 
-<<<<<<< HEAD
-static void intelli_plug_input_event(struct input_handle *handle,
-		unsigned int type, unsigned int code, int value)
-{
-#ifdef DEBUG_INTELLI_PLUG
-	pr_info("intelli_plug touched!\n");
-#endif
-
-	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_boost,
-		msecs_to_jiffies(10));
-}
-
-static int input_dev_filter(const char *input_dev_name)
-{
-	if (strstr(input_dev_name, "touchscreen") ||
-		strstr(input_dev_name, "sec_touchscreen") ||
-		strstr(input_dev_name, "touch_dev") ||
-		strstr(input_dev_name, "-keypad") ||
-		strstr(input_dev_name, "-nav") ||
-		strstr(input_dev_name, "-oj")) {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 0;
-	} else {
-		pr_info("touch dev: %s\n", input_dev_name);
-		return 1;
-	}
-}
-
-static int intelli_plug_input_connect(struct input_handler *handler,
-		struct input_dev *dev, const struct input_device_id *id)
-{
-	struct input_handle *handle;
-	int error;
-
-	if (input_dev_filter(dev->name))
-		return -ENODEV;
-
-	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = "intelliplug";
-
-	error = input_register_handle(handle);
-	if (error)
-		goto err2;
-
-	error = input_open_device(handle);
-	if (error)
-		goto err1;
-	pr_info("%s found and connected!\n", dev->name);
-	return 0;
-err1:
-	input_unregister_handle(handle);
-err2:
-	kfree(handle);
-	return error;
-}
-
-static void intelli_plug_input_disconnect(struct input_handle *handle)
-{
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
-}
-
-static const struct input_device_id intelli_plug_ids[] = {
-	{ .driver_info = 1 },
-	{ },
-};
-
-static struct input_handler intelli_plug_input_handler = {
-	.event          = intelli_plug_input_event,
-	.connect        = intelli_plug_input_connect,
-	.disconnect     = intelli_plug_input_disconnect,
-	.name           = "intelliplug_handler",
-	.id_table       = intelli_plug_ids,
-};
-
-int __init intelli_plug_init(void)
-{
-	int rc;
-
-	//pr_info("intelli_plug: scheduler delay is: %d\n", delay);
-	pr_info("intelli_plug: version %d.%d by faux123\n",
-		 INTELLI_PLUG_MAJOR_VERSION,
-		 INTELLI_PLUG_MINOR_VERSION);
-
-	rc = input_register_handler(&intelli_plug_input_handler);
-	intelliplug_wq = alloc_workqueue("intelliplug",
-				WQ_HIGHPRI | WQ_UNBOUND, 1);
-	intelliplug_boost_wq = alloc_workqueue("iplug_boost",
-				WQ_HIGHPRI | WQ_UNBOUND, 1);
-	INIT_DELAYED_WORK(&intelli_plug_work, intelli_plug_work_fn);
-	INIT_DELAYED_WORK(&intelli_plug_boost, intelli_plug_boost_fn);
-	queue_delayed_work_on(0, intelliplug_wq, &intelli_plug_work,
-		msecs_to_jiffies(10));
-
-#ifdef CONFIG_HAS_EARLYSUSPEND
-	register_early_suspend(&intelli_plug_early_suspend_struct_driver);
-=======
 int __init intelli_plug_init(void)
 {
 	/* We want all CPUs to do sampling nearly on same jiffy */
@@ -577,15 +210,16 @@ int __init intelli_plug_init(void)
 	if (num_online_cpus() > 1)
 		delay -= jiffies % delay;
 
-	pr_info("intelli_plug: scheduler delay is: %d\n", delay);
-	pr_info("intelli_plug: version %d by faux123\n", INTELLI_PLUG_VERSION);
+	//pr_info("intelli_plug: scheduler delay is: %d\n", delay);
+	pr_info("intelli_plug: version %d.%d by faux123\n",
+		 INTELLI_PLUG_MAJOR_VERSION,
+		 INTELLI_PLUG_MINOR_VERSION);
 
 	INIT_DELAYED_WORK(&intelli_plug_work, intelli_plug_work_fn);
 	schedule_delayed_work_on(0, &intelli_plug_work, delay);
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 	register_early_suspend(&intelli_plug_early_suspend_struct);
->>>>>>> 6738b18... intelli_plug: intelligent hotplug cpu driver with eco mode
 #endif
 	return 0;
 }
