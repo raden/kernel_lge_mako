8707b9ac98ba994808dec7d310c766ee491e8f14
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2014-07-22 11:04:32 +0800

    Update build file Seladang CM11 v10
    Signed-off-by: Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>

--- bina-stok.sh
+++ bina-stok.sh
@@ -2,14 +2,14 @@ export ARCH=arm
 unset CROSS_COMPILE
 #export CROSS_COMPILE=/opt/gcc-4.8-linaro/bin/arm-cortex_a15-linux-gnueabi- 
 #export CROSS_COMPILE=/opt/gcc-4.7-linaro/bin/arm-cortex_a15-linux-gnueabi- 
-export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
-
+#export CROSS_COMPILE=/opt/sabermod-gcc/bin/arm-eabi-
+export CROSS_COMPILE=/opt/gcc-linaro/arm-cortex_a15-linux-gnueabihf-linaro_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
 #echo "Cleaning old craps..."
 #make distclean
 
 export KBUILD_BUILD_USER=najmi
-export KBUILD_BUILD_HOST="ampng"
-export LOCALVERSION="-Seladang-AOSP-v8"
+export KBUILD_BUILD_HOST="ampang"
+export LOCALVERSION="-Seladang-CM11-v10"
 
 #echo "Copy backup config..."
 #cp najmi-mako-config .config
